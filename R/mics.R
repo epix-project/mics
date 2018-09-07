33bcc28c1972ba69742a35bd02e57f954b2ef660
Marc Choisy <marc.choisy@ird.fr>
2018-09-07 17:07:44 +0700

    Initial commit with package structure

---
+++ R/mics.R
@@ -0,0 +1,12 @@
+#' mics: analysing multiple indicator cluster surveys
+#'
+#' mics provides a collection of functions to analyse multiple indicator cluster
+#' surveys such as the UNICEF MICS or USAID DHS.
+#'
+#' To learn more about mics, start with the vignettes:
+#' `browseVignettes(package = "mics")`
+#'
+"_PACKAGE"
+
+# to quiet concerns of R CMD check for the values that appear in pipelines:
+if(getRversion() >= "2.15.1") utils::globalVariables(c(".", ":="))
