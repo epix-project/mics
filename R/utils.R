10d58314f7d249c38371fa67d156f59b45e4267f
Marc Choisy <marc.choisy@ird.fr>
2018-10-05 10:11:25 +0700

    get_code returns country code which is delete in split_code

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,13 @@
 get_code <- function(x) {
   x %>%
-    sub("^.*Filename=..", "", .) %>%
+    sub("^.*Filename=", "", .) %>%
     sub("\\.ZIP.*$"   , "", .)
 }
 
 
 split_code <- function(x) {
   require(magrittr) # %>%
+  x %<>% sub("^..", "", .) # the first 2 characters are the country code.
   sapply(seq(1, max(nchar(x)), 2), function(i) substr(x, i, i + 1)) %>%
     as.data.frame() %>%
     setNames(c("file_type", "version_number", "file_format"))
