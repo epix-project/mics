7852e62da50164bec2cfa3cadfaa57b9f94e9ced
Marc Choisy <marc.choisy@ird.fr>
2018-10-05 10:39:55 +0700

    Adding the documentation to lodown2

--- R/lodown2.R
+++ R/lodown2.R
@@ -1,5 +1,14 @@
+#' Locally Download Publicly-available data
 #'
+#' \code{lodown2} is a wrapper around the \code{\link[lodown]{lodown}} function
+#' that checks the existance of files on disk before trying to download them. It
+#' will thus download only the files that are not already on disk.
 #'
+#' See examples in the \code{\link{with_profile}} documentation.
+#'
+#' @param catalog \code{data.frame} detailing available microdata extracts and
+#' metadata, see the documentation of \code{\link[lodown]{lodown}}.
+#' @param ... additional arguments passed to \code{\link[lodown]{lodown}}.
 #'
 #' @importFrom lodown lodown
 #' @importFrom magrittr %>% %<>%
