6971b4bb5d7ea66ae2f8f48453146048df4825d3
Marc Choisy <marc.choisy@ird.fr>
2018-10-08 14:29:32 +0700

    Replacing message by cat

--- R/load_dhs.R
+++ R/load_dhs.R
@@ -66,13 +66,13 @@ load_dhs <- function(profile, ..., output = getwd()) {
   catalog <- get_catalog2(profile)
   if (is.character(catalog)) {
     if (length(catalog) > 1) stop("There are already more than one catalog in the global environment that corresponds to this profile's project.")
-    message(paste("There is already the following catalog in the global environment that corresponds to this profile's project:", catalog,
+    cat(paste("There is already the following catalog in the global environment that corresponds to this profile's project:", catalog,
                   "\nWe will use this catalog to download the data if there are not already downloaded."))
     catalog <- get(catalog, .GlobalEnv)
   } else {
     catalog_name <- paste0(as.list(match.call())$profile, "_catalog")
     assign(catalog_name, catalog, envir = .GlobalEnv)
-    message(paste0("The catalog has been save in the global environment under the name of '", catalog_name, "'."))
+    cat(paste0("The catalog has been save in the global environment under the name of '", catalog_name, "'."))
   }
   catalog %>%
     filter(...) %>%
@@ -85,7 +85,7 @@ load_dhs <- function(profile, ..., output = getwd()) {
   if (any(sel))
     for(i in 1:sum(sel))
       assign(data_objects[sel][i], readRDS(data_files[sel][i]), envir = .GlobalEnv)
-  else message(paste("Data are already available in the global environment:\n", paste(data_objects[! sel], sep = ", ")))
+  else cat(paste("Data are already available in the global environment:\n", paste(data_objects[! sel], sep = ", ")))
 }
 
 
