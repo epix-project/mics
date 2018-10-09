c3496022909cd6dee7962462ed2c5c7578f35046
Marc Choisy <marc.choisy@ird.fr>
2018-10-09 15:58:59 +0700

    Fixing bug in load_dhs

--- R/load_dhs.R
+++ R/load_dhs.R
@@ -64,27 +64,28 @@
 #'
 load_dhs <- function(profile, ..., output = getwd()) {
   catalog <- get_catalog2(profile)
-  if (is.character(catalog)) {
-    if (length(catalog) > 1) stop("There are already more than one catalog in the global environment that corresponds to this profile's project.")
-    cat(paste("There is already the following catalog in the global environment that corresponds to this profile's project:", catalog,
-                  "\nWe will use this catalog to download the data if there are not already downloaded."))
-    catalog <- get(catalog, .GlobalEnv)
-  } else {
+#  if (is.character(catalog)) {
+#    if (length(catalog) > 1) stop("There are already more than one catalog in the global environment that corresponds to this profile's project.")
+#    cat(paste("There is already the following catalog in the global environment that corresponds to this profile's project:", catalog,
+#                  "\nWe will use this catalog to download the data if there are not already downloaded."))
+#    catalog <- get(catalog, .GlobalEnv)
+#  } else {
     catalog_name <- paste0(as.list(match.call())$profile, "_catalog")
     assign(catalog_name, catalog, envir = .GlobalEnv)
     cat(paste0("The catalog has been save in the global environment under the name of '", catalog_name, "'."))
-  }
+#  }
   catalog %>%
     filter(...) %>%
-    mutate(output_folder = output) %>%
-    with_profile(profile, lodown2)(catalog = .)
+    mutate2(output_folder = output) %>%
+#    with_profile(profile, lodown2)(catalog = .)
+    lodown2()
 
   data_files <- grep("FL.rds", dir(output), value = TRUE)
   data_objects <- sub(".rds", "", data_files)
   sel <- ! data_objects %in% ls(.GlobalEnv)
   if (any(sel))
     for(i in 1:sum(sel))
-      assign(data_objects[sel][i], readRDS(data_files[sel][i]), envir = .GlobalEnv)
+      assign(data_objects[sel][i], readRDS(paste0(output, "/", data_files[sel][i])), envir = .GlobalEnv)
   else cat(paste("Data are already available in the global environment:\n", paste(data_objects[! sel], sep = ", ")))
 }
 
